018c67ec56aa475a9ebd542a826f5d155a450ef6
Ratko Ćosić <ratko_cosic@hotmail.com>
2025-10-02 17:59:11 +0200

    Add project files.

---
+++ Algebra.WebShop2025.App/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Algebra.WebShop2025.App.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
---
+++ Algebra.WebShop2025.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Algebra.WebShop2025.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
