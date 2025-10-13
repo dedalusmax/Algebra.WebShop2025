9a9b9f8d9c0658e47f11bb8d37439ed4ebd28d1e
Ratko Ćosić <ratko_cosic@hotmail.com>
2025-10-13 20:21:42 +0200

    products easy

---
+++ Algebra.WebShop2025.App/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using Algebra.WebShop2025.App.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Algebra.WebShop2025.App.Controllers;
+
+public class ProductsController(ApplicationDbContext context) : Controller
+{
+    public IActionResult Index()
+    {
+        var products = context.Products.ToList();
+
+        return View(products);
+    }
+}
