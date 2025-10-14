8f1b38359d19e0b7cbf32598c1ccf4aaf9c3e2ca
Ratko Ćosić <ratko_cosic@hotmail.com>
2025-10-14 20:21:55 +0200

    cart

---
+++ Algebra.WebShop2025.App/Controllers/CartController.cs
@@ -0,0 +1,17 @@
+﻿using Algebra.WebShop2025.App.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Algebra.WebShop2025.App.Controllers;
+
+[Authorize]
+public class CartController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var cart = HttpContext.Session.GetCart();
+
+        return View(cart);
+    }
+}
