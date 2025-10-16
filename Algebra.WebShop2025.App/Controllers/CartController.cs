980db7db68004019b62c5364ed7dca6fa9b1929a
Ratko Ćosić <ratko_cosic@hotmail.com>
2025-10-16 19:04:05 +0200

    cart view

--- Algebra.WebShop2025.App/Controllers/CartController.cs
+++ Algebra.WebShop2025.App/Controllers/CartController.cs
@@ -22,33 +22,39 @@ public class CartController(ApplicationDbContext context) : Controller
     {
         var cart = HttpContext.Session.GetCart();
 
-        if (cart.Items.Count == 0)
-        {
-            var item = new CartItem
-            {
-                Product = context.Products.Find(productId)!,
-                Quantity = 1
-            };
+        var existingItem = cart.Items.SingleOrDefault(x => x.Product.Id == productId);
 
-            cart.Items.Add(item);
+        if (existingItem != null)
+        {
+            existingItem.Quantity++;
         }
         else
         {
-            if (cart.Items.Any(x => x.Product.Id == productId))
+            var product = context.Products.Find(productId);
+            if (product != null)
             {
-                var item = cart.Items.Single(x => x.Product.Id == productId);
-                item.Quantity++;
-            }
-            else
-            {
-                var item = new CartItem
+                cart.Items.Add(new CartItem
                 {
-                    Product = context.Products.Find(productId)!,
+                    Product = product,
                     Quantity = 1
-                };
-
-                cart.Items.Add(item);
+                });
             }
+        }
+
+        HttpContext.Session.SetCart(cart);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    public IActionResult RemoveFromCart(int productId)
+    {
+        var cart = HttpContext.Session.GetCart();
+
+        if (cart.Items.Any(x => x.Product.Id == productId))
+        {
+            var item = cart.Items.Single(x => x.Product.Id == productId);
+            cart.Items.Remove(item);
         }
 
         HttpContext.Session.SetCart(cart);
