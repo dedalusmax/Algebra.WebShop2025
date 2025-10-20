f1beede0668953383f32e239dac581933bf9f668
Ratko Ćosić <ratko_cosic@hotmail.com>
2025-10-20 20:50:41 +0200

    view component

---
+++ Algebra.WebShop2025.App/Views/Shared/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using Algebra.WebShop2025.App.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Algebra.WebShop2025.App.Views.Shared;
+
+//[ViewComponent(Name = "Cart")]
+//[NonViewComponent]
+public class ShoppingCart : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        var cart = HttpContext.Session.GetCart();
+
+        return View(cart);
+    }
+}
